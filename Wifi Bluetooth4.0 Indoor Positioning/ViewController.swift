3c0e84269c4b39ab60ad082b26b6e0879111388a
ceciliawu <cecilia@ece.utoronto.ca>
2018-01-09 22:48:46 -0500

    Initial Commit

---
+++ Wifi Bluetooth4.0 Indoor Positioning/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Wifi Bluetooth4.0 Indoor Positioning
+//
+//  Created by Wu Cecilia on 2018-01-09.
+//  Copyright © 2018 ece496.team2017728. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    //MARK: Properties
+    @IBOutlet weak var wifiStartLabel: UILabel!
+    @IBOutlet weak var wifiStartButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: Actions
+    @IBOutlet weak var wifiStartButtonActions: UIButton!
+    
+}
+
+
