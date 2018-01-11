579bc60f1347f3d24e095ad36296b21639c9a335
ceciliawu <cecilia@ece.utoronto.ca>
2018-01-11 02:04:22 -0500

    Fix Object Outlet Dependencies

--- Wifi Bluetooth4.0 Indoor Positioning/ViewController.swift
+++ Wifi Bluetooth4.0 Indoor Positioning/ViewController.swift
@@ -23,8 +23,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    //MARK: Actions
-    @IBOutlet weak var wifiStartButtonActions: UIButton!
     
 }
 
